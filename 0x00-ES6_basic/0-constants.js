e7e30ad4fe4b9845da46c609f8e89419ea29546c
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-09-26 13:12:58 +0100

    Initial Commit

---
+++ 0x00-ES6_basic/0-constants.js
@@ -0,0 +1,14 @@
+const taskFirst = () => {
+  const task = 'I prefer const when I can.';
+  return task;
+}
+
+export function getLast() {
+  return ' is okay';
+}
+
+const taskNext = () => {
+  let combination = 'But sometimes let';
+  combination += getLast();
+  return combination;
+}
