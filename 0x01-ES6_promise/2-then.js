4aef8126e2461e3d682dd85752f54255869334cb
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-09-28 21:17:20 +0100

    revamped

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,13 +1,5 @@
 export default function handleResponseFromAPI(promise) {
   promise.then(() => {
-    return {
-      status: 200,
-      body: 'Succes',
-    };
-  }).then((res) => {
     console.log('Got a response from the API');
-    return res;
-  }).catch((error) => {
-    Error(error);
-  });
+  }, (error) => error);
 }
