2f5f3a56547ad022a72ea3ddb19ce4b77523ce6a
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-09-28 18:09:42 +0100

    1-promise.js

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -2,11 +2,11 @@ export default function getFullResponseFromAPI(success) {
   return new Promise((res, rej) => {
     if (success) {
       res({
-	  [status]: 200,
-	  [body]: 'Success',
+	  status: 200,
+	  body: 'Success',
 	});
     } else {
-      rej('The fake API is not working currently');
+      rej(Error('The fake API is not working currently'));
     }
   });
 }
