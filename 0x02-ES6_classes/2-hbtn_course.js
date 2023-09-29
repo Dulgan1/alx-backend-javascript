16f7952fef246a1a5909285085f1d421f6d2803f
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-09-29 11:42:13 -0700

    ESLint

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -11,31 +11,37 @@ export default class HolbertonCourse {
     this._length = length;
     this._students = students;
   }
+
   get name() {
-    return this._name
+    return this._name;
   }
+
   get length() {
-    return this._length
+    return this._length;
   }
+
   get students() {
-    return this._students
+    return this._students;
   }
+
   set name(name) {
     if (typeof name !== 'string') {
       throw new TypeError('Name must be a string');
     }
-    this._name = name
+    this._name = name;
   }
+
   set length(length) {
     if (typeof length !== 'number') {
-      throw new TypeError('Length must be a number')
+      throw new TypeError('Length must be a number');
     }
-    this._length = length
+    this._length = length;
   }
+
   set students(students) {
     if (students.every((s) => typeof s !== 'string') || !Array.isArray(students)) {
-      throw new TypeError('Students must be an array of strings')
+      throw new TypeError('Students must be an array of strings');
     }
-    this._students = students
+    this._students = students;
   }
 }
--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -3,16 +3,20 @@ export default class Currency {
     this._code = code;
     this._name = name;
   }
+
   get code() {
-    return this._code
+    return this._code;
   }
+
   get name() {
-    return this._name
+    return this._name;
   }
+
   set code(code) {
-    this._code = code
+    this._code = code;
   }
+
   set name(name) {
-    this._name = name
+    this._name = name;
   }
 }
