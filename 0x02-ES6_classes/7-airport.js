2d087c93a9873f3c70acad582a50ef8efc199454
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-09-30 23:26:40 +0100

    7-airport.js

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,39 @@
+class Airport {
+  constructor(name, code) {
+    if (typeof name === 'string' && typeof code === 'string') {
+      this._name = name;
+      this._code = code;
+    } else {
+      throw new TypeError('Name and Code must be string');
+    }
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set name(name) {
+    if (typeof name === 'string') {
+      this._name = name;
+    } else {
+      throw new TypeError('Name must be a string');
+    }
+  }
+
+  set code(code) {
+    if (typeof code === 'string') {
+      this._code = code;
+    } else {
+      throw new TypeError('Code must be a string');
+    }
+  }
+
+  toString() {
+    return `${this._code}`;
+  }
+}
+
