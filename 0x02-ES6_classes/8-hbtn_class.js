93bcec01b459f71928c1c692bde9fc1606adf3d2
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-09-30 23:41:14 +0100

    Initial commit

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,27 @@
+export default class HolbertonClass {
+  constructor(size, location) {
+    if (typeof size === 'number' && typeof location === 'string') {
+      this._size = size;
+      this._location = location;
+    } else {
+      throw new TypeError('Size must be a number and Location must be a string');
+    }
+  }
+
+  get size() {
+    return this._size;
+  }
+
+  get location() {
+    return this._location;
+  }
+
+  valueOf() {
+    return this._size;
+  }
+
+  toString() {
+    return this._location;
+  }
+}
+
