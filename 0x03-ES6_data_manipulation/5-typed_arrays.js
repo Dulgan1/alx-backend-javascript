749aa2fd66e5247049d2a323076054aa03cd78e8
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-10-04 01:20:11 +0100

    passed value as array

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -2,6 +2,6 @@ export default function createInt8TypedArray(length, position, value) {
   if (position > length - 1) throw new Error('Position outside range');
   const buffer = ArrayBuffer(length);
   const view = Int8Array(buffer);
-  view.set(value, position);
+  view.set([value], position);
   return DataView(buffer);
 }
