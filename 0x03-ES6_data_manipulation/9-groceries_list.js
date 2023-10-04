fbc26a104df18be367cb8ed1dc136bb7747a50aa
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-10-04 02:12:06 +0100

    Typo fix

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -2,8 +2,8 @@ export default function groceriesList() {
   const list = new Map();
   list.set('Apples', 10);
   list.set('Tomatoes', 10);
-  list('Pasta', 1);
-  list('Rice', 1);
-  list('Banana', 5);
+  list.set('Pasta', 1);
+  list.set('Rice', 1);
+  list.set('Banana', 5);
   return list;
 }
