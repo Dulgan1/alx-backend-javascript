538c230f1e03b26a0c513f24ca24d3aad4689509
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-10-04 01:09:01 +0100

    testing

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,5 +1,5 @@
 export default function createInt8TypedArray(length, position, value) {
-  if (position > length - 1) throw new Error('Position outside range');
+  if (position < length - 1) throw new Error('Position outside range');
   const buffer = ArrayBuffer(length);
   const view = Int8Array(buffer);
   view[position] = value;
