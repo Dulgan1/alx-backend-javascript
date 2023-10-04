ecee7743bf172e6f49b6a7d3105131c9fe022e31
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-10-04 13:29:11 +0100

    Element check

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,5 @@
 export default function cleanSet(set, startString) {
   if (!startString || startString.length === 0 || typeof startString !== 'string' || startString === '' || typeof set !== 'object') return '';
-  const dashString = [...set].filter((elem) => elem !== 'undefined').filter((elem) => elem.indexOf(startString) === 0).map((elem) => elem.replace(startString, ''));
+  const dashString = [...set].filter((elem) => elem).filter((elem) => elem.indexOf(startString) === 0).map((elem) => elem.replace(startString, ''));
   return dashString.join('-');
 }
