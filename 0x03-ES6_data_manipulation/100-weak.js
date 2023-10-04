4a45d1fa9634293d2e45854e29d1c3a9f529b055
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-10-04 14:15:20 +0100

    export fix

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,5 +1,5 @@
 export const weakMap = new WeakMap();
-export default function queryAPI(endpoint) {
+export function queryAPI(endpoint) {
   if (weakMap.size >= 5) throw new Error('Endpoint load is high');
   weakMap.set(endpoint);
 }
