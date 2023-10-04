a697763e1cd71e20385a960fe1b84fc6c57dc6fb
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-10-04 15:09:34 +0100

    This could be worst :D

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,5 +1,9 @@
 export const weakMap = new WeakMap();
 export function queryAPI(endpoint) {
   if (weakMap.get(endpoint) >= 5) throw new Error('Endpoint load is high');
-  weakMap.set(endpoint, new Number(weakMap.get(endpoint))+1);
+  if (weakMap.get(endpoint)) {
+    weakMap.set(endpoint, weakMap.get(endpoint)+1);
+  } else {
+    weakMap.set(endpoint, 0);
+  }
 }
