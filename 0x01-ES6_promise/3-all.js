79760029c400f15ec2d78c704cc4611036ee4180
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-10-22 12:51:51 +0100

    initial commit

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,11 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  Promise.all([uploadPhoto(), createUser()]).then(
+    (resolve) => {
+      console.log(`${resolve[0].body} ${resolve[1].firstName} ${resolve[1].lastName}`);
+    }, () => {
+      console.log('Signup system offline');
+    }
+  );
+}
