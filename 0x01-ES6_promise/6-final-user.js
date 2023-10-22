427abdd564c4b383e2cacfa88d573b20954d7596
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-10-22 15:46:53 +0100

    IC

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,14 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const response = [];
+  try {
+    const user = await signUoUser(firstName, lastName);
+    await uploadPhoto(fileName);
+    response.push({ status: 'fulfilled', value: user, });
+  } catch (error) {
+    response.push({ status: 'rejected', value: `Error: ${fileName} cannot be processed`, });
+  }
+  return response;
+}
