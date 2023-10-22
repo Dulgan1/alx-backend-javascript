6eb66f5978a23fc803a00434519e506ac1163212
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-10-22 20:52:50 +0100

    ESlint

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,7 +1,6 @@
 export default function divideFunction(numerator, denominator) {
   if (!denominator) {
     return Error('cannot divide by 0');
-  } else {
-    return numerator / denominator;
   }
+  return numerator / denominator;
 }
