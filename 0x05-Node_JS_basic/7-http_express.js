0f0ff240daf9aaf3c736710d5741dc2bfc5fe309
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-11-26 02:41:22 +0100

    revert to old method

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -14,7 +14,7 @@ app.get('/', (req, res) => {
 app.get('/students', async (req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.send('This is the list of our students\n');
+  res.write('This is the list of our students\n');
   await students(process.argv[2]).then((data) => {
     res.write(`Number of students: ${data.students.length}\n`);
     res.write(`Number of students in CS: ${data.csStudents.length}. List: ${data.csStudents.join(', ')}\n`);
